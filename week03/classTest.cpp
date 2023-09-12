219398736b6436d23fb91a9cfa48c9a296c82f6b
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-09-12 13:49:28 +0900

    for and if statement -> even/odd sum

--- week03/classTest.cpp
+++ week03/classTest.cpp
@@ -2,11 +2,22 @@
 using namespace std;
 
 int main(void) {
-	int sum = 0;
-	for ( int i = 1; i <= 10; i++) {
-		cout << "i= " << i << endl;
-		sum = sum + i;
+	int sumEven = 0;
+	int sumOdd = 0;
+	for ( int i = 1; i <= 100; i++) {
+		if (i % 2 == 0)
+		{
+			cout << "even" <<  i << endl;
+			sumEven = sumEven + i;
+		}
+		else
+		{
+			cout <<  "odd" << i << endl;
+			sumOdd = sumOdd + i;
+		}
 	}
-	cout << "sum= " << sum << endl;
+	cout << "Sum of Even= " << sumEven << endl;
+
+	cout << "Sum of Odd= " << sumOdd << endl;
 	return 0;
 }
