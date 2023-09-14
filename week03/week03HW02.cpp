8ae9fa7a2e04a25d3bc10bb2b82efa416333a7d0
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-09-14 16:06:45 +0900

    week03 : hw03 : Ascii 대칭 출력

--- week03/week03HW02.cpp
+++ week03/week03HW02.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 using namespace std;
 
-//int main(void)
-//{
-//	for (int i = 65; i >=0; i--) {
-//		cout << char(i) << endl;
-//	}
-//}
+#include <iomanip>
+
+int main(void)
+{
+	for (int i = 0; i < 8; i++) {
+		for (int j = 65; j < 72 - i; j++) {
+			cout <<  char(j) << " ";
+		}
+		for (int k = 0; k < i; k++) {
+			cout << "    ";
+		}
+
+		for (int j = 71-i; j >= 65; j--) {
+			cout << char(j) << " ";
+
+		}
+		cout << endl;
+	}
+	
+}
