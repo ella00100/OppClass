b9287cf8744a6f8d8e1ebb81ddd33051bec5307f
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-09-19 12:21:49 +0900

    week04 switch ex03: default

--- week04/ex02.cpp
+++ week04/ex02.cpp
@@ -1,25 +1,25 @@
-#include <iostream>
-using namespace std;
-
-int main(void)
-{
-	int c = 3;
-
-	switch (c)
-	{
-	case 'v':
-		cout << "I am in case v" << endl;
-		break;
-	case 3:
-		cout << "I am in case 3" << endl;
-		break;
-	case 12:
-		cout << "I am in case 12" << endl;
-		break;
-	default:
-		cout << "I am in default" << endl;
-	}
-
-	return 0;
-
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main(void)
+//{
+//	int c = 3;
+//
+//	switch (c)
+//	{
+//	case 'v':
+//		cout << "I am in case v" << endl;
+//		break;
+//	case 3:
+//		cout << "I am in case 3" << endl;
+//		break;
+//	case 12:
+//		cout << "I am in case 12" << endl;
+//		break;
+//	default:
+//		cout << "I am in default" << endl;
+//	}
+//
+//	return 0;
+//
+//}
