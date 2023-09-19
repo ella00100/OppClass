cbd6e4b6d39183b28d2d5c56dda0bec20491b3c8
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-09-19 13:26:08 +0900

    week04 hw03 : switch-case -> just int

---
+++ week04/hw03.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	float a = 3.5;
+
+	switch (int(a)) {
+	case (int(0.5)):
+		cout << "The art of C" << endl;
+		break;
+	case (int(1.5)):
+		cout << "The spirit of C" << endl;
+		break;
+	case (int(2.5)):
+		cout << "See through C" << endl;
+		break;
+	case (int(3.5)):
+		cout << "Simply c" << endl;
+	}
+	return 0;
+}
