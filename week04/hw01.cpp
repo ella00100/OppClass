1d92a94d3310daa06865572f1f764c5f940d2c6d
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-09-19 12:37:51 +0900

    week04 hw01 : suite

---
+++ week04/hw01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int suite = 1;
+	switch (suite)
+	{
+	case 0:
+		cout << "Club" << endl;
+	case 1:
+		cout << "Diamond" << endl;
+	}
+	return 0;
+}
