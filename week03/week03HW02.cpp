d5823207abd662f83f1eb32c837aed8efa926261
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-09-19 10:39:13 +0900

    week03 HW: for, do-while, while

--- week03/week03HW02.cpp
+++ week03/week03HW02.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
 using namespace std;
 
-#include <iomanip>
-
 int main(void)
 {
-	for (int i = 0; i < 8; i++) {
+	for (int i = 0; i < 8; i++) { 
 		for (int j = 65; j < 72 - i; j++) {
 			cout <<  char(j) << " ";
 		}
-		for (int k = 0; k < i; k++) {
-			cout << "    ";
+
+		for (int k = 1; k < i*2; k++) {
+			cout << "  ";
 		}
 
-		for (int j = 71-i; j >= 65; j--) {
-			cout << char(j) << " ";
+		if (i == 0) {
+			for (int j = 70 - i; j >= 65 ; j--) {
+				cout << char(j) << " ";
+			}
+		} else{
+			
+			for (int j = 71 - i; j >= 65; j--) {
+				cout << char(j) << " ";
 
+			}
 		}
+	
 		cout << endl;
 	}
 	
