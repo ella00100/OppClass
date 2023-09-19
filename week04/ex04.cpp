aec9c566e62212c4cb337be5c97d5f0601af16b2
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-09-19 12:24:06 +0900

    week04 ex04 : satify first case

---
+++ week04/ex04.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int i = 0;
+	switch (i)
+	{
+	case 0:
+		cout << "Customers are dicey" << endl;
+	case 1:
+		cout << "Markets are pricey" << endl;
+	case 2:
+		cout << "Investors are moody" << endl;
+	case 3:
+		cout << "At least employees are good" << endl;
+	}
+	return 0;
+}
