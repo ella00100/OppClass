fee1b651b59a6e47a535789b91e07f4fe4e79e9b
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-09-30 17:42:09 +0900

    week05 ex1 : add func

---
+++ week5/ex1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b);
+
+void main() {
+	int x = 1, y = 2, z;
+	z = add(x, y);
+	cout << "z= " << z << endl;
+}
+
+int add(int a, int b) {
+	return (a + b);
+}
