b363f34814ab876d39e1c42fc353dff042ac72cd
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-10-12 12:26:15 +0900

    week07 : ex05 Access the wrong index

---
+++ week07/ex05.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#define SIZE 5
+
+int main() {
+	int array[SIZE] = { 1,2,3,4,5};
+	int i;
+
+	for (i = 0; i <= SIZE; i++)
+		cout << "array[" << i << "]" << "\t" << array[i] << endl;
+
+	return 0;
+}
