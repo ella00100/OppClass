01a4f4355cfaf6b453a599ff7eca63215bfe9f2c
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-10-30 14:25:56 +0900

    mid Project 01: correlation codefficinet r

--- Project1/Project01.cpp
+++ Project1/Project01.cpp
@@ -12,27 +12,31 @@ int main(void) {
 	double  r;
 	double  sum_multi_val;
 	double  multi_each_sum_val;
-	double  x_sum_square, y_sum_square;
-	double  x_square_sum, y_square_sum;
+	double  sum_square_x, sum_square_y;
+	double  square_sum_x, square_sum_y;
 
 	double  x[SIZE] = { 34.22, 39.87, 41.85, 43.23, 40.06,
 	53.29, 53.29, 54.14, 49.12, 40.71, 55.15 };
-	
+
 	double  y[SIZE] = { 102.43, 100.93, 97.43, 97.81, 98.32,
 	98.32, 100.07, 97.08, 91.59, 94.85, 94.65 };
 
 	sum_multi_val = sum_multi_xy(x, y);
 	multi_each_sum_val = multi_each_sum(x, y);
-	x_sum_square = sum_square(x);
-	y_sum_square = sum_square(y);
-	x_square_sum = square_sum(x);
-	y_square_sum = square_sum(y);
+	sum_square_x = sum_square(x);
+	cout << "sigma (x^2) : " << sum_square_x << endl;
+	sum_square_y = sum_square(y);
+	cout << "sigma (y^2) : " << sum_square_y << endl;
+	square_sum_x = square_sum(x);
+	cout << "(sigma x)^2 : " << square_sum_x << endl;
+	square_sum_y = square_sum(y);
+	cout << "(sigma y)^2 : " << square_sum_y << endl;
 
 
 	r = (SIZE * sum_multi_val - multi_each_sum_val) /
-		sqrt((SIZE * x_sum_square - x_square_sum) * (SIZE*y_sum_square - y_square_sum));
+		sqrt((SIZE * sum_square_x - square_sum_x) * (SIZE * sum_square_y - square_sum_y));
 
-	cout << "correlation coeficient r is " << r << endl;
+	cout << "correlation coefficient r : " << r << endl;
 	return 0;
 }
 
@@ -43,18 +47,19 @@ double  sum_multi_xy(double  arr_x[], double  arr_y[]) {
 		multi_xy_list[i] = arr_x[i] * arr_y[i];
 		sum += multi_xy_list[i];
 	}
+	cout << "sigma xy : " << sum << endl;
 	return sum;
 }
 
 double  multi_each_sum(double  arr_x[], double  arr_y[]) {
-	double  sum_x=0, sum_y=0;
+	double  sum_x = 0, sum_y = 0;
 	double  multi_sum = 0;
 	for (int i = 0; i < SIZE; i++) {
 		sum_x += arr_x[i];
 		sum_y += arr_y[i];
 	}
 	multi_sum = sum_x * sum_y;
-	cout << multi_sum << endl;
+	cout << "sigma x * sigma y : " << multi_sum << endl;
 	return multi_sum;
 }
 
